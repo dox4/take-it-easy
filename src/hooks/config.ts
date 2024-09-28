880b9429849eb839619896c188cfa3fd6482b3d3
dox4 <dox4@foxmail.com>
2024-09-28 23:35:07 +0800

    fix: add type notation

--- src/hooks/config.ts
+++ src/hooks/config.ts
@@ -1,9 +1,9 @@
 import { enUS, zhCN, darkTheme } from "naive-ui"
-import type { GlobalTheme } from "naive-ui"
+import type { NLocale, GlobalTheme } from "naive-ui"
 
-import { ref } from "vue"
-const theme = ref<GlobalTheme | null>(null)
-const lang = ref(zhCN)
+import { ref, type Ref } from "vue"
+const theme: Ref<GlobalTheme | null> = ref<GlobalTheme | null>(null)
+const lang: Ref<NLocale> = ref(zhCN)
 const changeTheme = () => {
   if (theme.value === null) {
     theme.value = darkTheme as GlobalTheme
